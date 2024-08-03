5bf5688f0c691fb20b5122728a57a535ccde6c4d
Dmitry Maximyuk <66935827+dmaximyuk@users.noreply.github.com>
2024-08-03 12:50:52 +0500

    Working...

--- src/modals/ModalProvider/ModalProvider.tsx
+++ src/modals/ModalProvider/ModalProvider.tsx
@@ -1,10 +1,7 @@
 import "./ModalProvider.sass";
 
 import { ModalProviderProps } from "./ModalProvider.interface";
-import { useModal } from "hooks";
 
 export const ModalProvider: ModalProviderProps = (activeModal) => {
-  const { closeModal } = useModal();
-
   return <></>;
 };
