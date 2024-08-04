d317706b64f6dd476c9a334d6402af4db7e414a4
Dmitry Maximyuk <66935827+dmaximyuk@users.noreply.github.com>
2024-08-04 13:03:03 +0500

    Refactoring

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from "vite";
+import path from "node:path";
 
 import react from "@vitejs/plugin-react-swc";
 import tsconfigPaths from "vite-tsconfig-paths";
@@ -9,6 +10,11 @@ import { compilerOptions } from "./tsconfig.json";
 export default defineConfig({
   appType: "spa",
   publicDir: "public",
+  resolve: {
+    alias: {
+      "@styles": path.resolve(__dirname, "src/styles"),
+    },
+  },
   build: {
     sourcemap: false,
     cssCodeSplit: true,
